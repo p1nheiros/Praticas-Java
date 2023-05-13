0a586cd62c148acf00540ed8a817e5b044b1368a
p1nheiros <pinheiros.dev@gmail.com>
2023-05-12 21:38:09 -0300

    atualização ordenar numeros

--- Ordena Numeros/src/ordenaNumeros.java
+++ Ordena Numeros/src/ordenaNumeros.java
@@ -1,27 +1,106 @@
-import java.util.ArrayList; // armazena os números
-import java.util.Collections; // ordena os números
-import java.util.Scanner; // lê a entrada do usuário
+import java.util.Scanner;
 
 public class ordenaNumeros {
+
     public static void main(String[] args) {
-        ArrayList<Integer> numeros = new ArrayList<>(); // armazena números inteiros
-        Scanner scanner = new Scanner(System.in); // criação de um objeto de leitura associado a variavel scanner
+        int n1, n2, n3, n4, n5, aux;
+        Scanner scanner = new Scanner(System.in);
 
-        // adiciona os números inseridos no ArrayList
         System.out.println("Escreva o primeiro número: ");
-        numeros.add(scanner.nextInt());
+        n1 = scanner.nextInt();
+        while (n1 < 0) {
+            System.out.println("O valor inserido deve ser positivo. Insira um novo valor:");
+            n1 = scanner.nextInt();
+        }
+
         System.out.println("Escreva o segundo número: ");
-        numeros.add(scanner.nextInt());
+        n2 = scanner.nextInt();
+        while (n2 < 0) {
+            System.out.println("O valor inserido deve ser positivo. Insira um novo valor:");
+            n2 = scanner.nextInt();
+        }
+
         System.out.println("Escreva o terceiro número: ");
-        numeros.add(scanner.nextInt());
+        n3 = scanner.nextInt();
+        while (n3 < 0) {
+            System.out.println("O valor inserido deve ser positivo. Insira um novo valor:");
+            n3 = scanner.nextInt();
+        }
+
         System.out.println("Escreva o quarto número: ");
-        numeros.add(scanner.nextInt());
+        n4 = scanner.nextInt();
+        while (n4 < 0) {
+            System.out.println("O valor inserido deve ser positivo. Insira um novo valor:");
+            n4 = scanner.nextInt();
+        }
+
         System.out.println("Escreva o quinto número: ");
-        numeros.add(scanner.nextInt());
+        n5 = scanner.nextInt();
+        while (n5 < 0) {
+            System.out.println("O valor inserido deve ser positivo. Insira um novo valor:");
+            n5 = scanner.nextInt();
+        }
 
-        Collections.sort(numeros); // ordena os números do ArrayList
+        // Definindo n1 como menor valor
+        if (n2 < n1) {
+            aux = n1;
+            n1 = n2;
+            n2 = aux;
+        }
+        if (n3 < n1) {
+            aux = n1;
+            n1 = n3;
+            n3 = aux;
+        }
+        if (n4 < n1) {
+            aux = n1;
+            n1 = n4;
+            n4 = aux;
+        }
+        if (n5 < n1) {
+            aux = n1;
+            n1 = n5;
+            n5 = aux;
+        }
 
-        System.out.println("Os números em ordem crescente: " + numeros);
+        // Definindo n2 como o segundo menor valor
+        if (n3 < n2) {
+            aux = n2;
+            n2 = n3;
+            n3 = aux;
+        }
+        if (n4 < n2) {
+            aux = n2;
+            n2 = n4;
+            n4 = aux;
+        }
+        if (n5 < n2) {
+            aux = n2;
+            n2 = n5;
+            n5 = aux;
+        }
+
+        // Definindo n3 como o terceiro menor valor
+        if (n4 < n3) {
+            aux = n3;
+            n3 = n4;
+            n4 = aux;
+        }
+        if (n5 < n3) {
+            aux = n3;
+            n3 = n5;
+            n5 = aux;
+        }
+
+        // Definindo o maior valor
+        if (n5 < n4) {
+            aux = n4;
+            n4 = n5;
+            n5 = aux;
+        }
+
+        System.out.println(
+                "Os valores ordenados de forma crescente são: " + n1 + "," + n2 + "," + n3 + "," + n4 + "," + n5);
 
         scanner.close();
     }
