f9567ce0a9b70e4898fc9dc930cb25236d67b18c
oliveeiralucas <lucas.oliveira@edu.unifil.br>
2023-06-06 16:44:57 -0300

    subindo arquivos do projeto

--- intellij/ProjetosJava/src/com/company/trabalho.java
+++ intellij/ProjetosJava/src/com/company/trabalho.java
@@ -11,6 +11,13 @@ public class trabalho {
         System.out.println("\nInforme o tamanho do vetor:");
         tamanho = scanner.nextInt();
 
+        //garante que o vetor tenha um comprimento inteiro e positivo
+        while (tamanho <= 0 || tamanho % 1  != 0){
+            System.out.println("Voce inseriu um tamanho inválido para o vetor, insira um valor correto!");
+            System.out.println("\nInforme um tamanho válido para o vetor:");
+            tamanho = scanner.nextInt();
+        }
+
         //int [] vetor;
         String[] vetor = new String[tamanho];
 
@@ -27,28 +34,109 @@ public class trabalho {
             System.out.println("Informe a operação desejada:");
             operacao = scanner.nextInt();
 
-            if (operacao == 1) {
-                //operacao 1.
-                for (int i = 0; i < tamanho; i++) {
-                    System.out.println("Insira um valor no vetor na posição " + i + ".");
-                    vetor[i] = scanner.next();
+            switch (operacao) {
+                case 1:
+                    // Operação 1.
+                    for (int i = 0; i < tamanho; i++) {
+                        System.out.println("Insira um valor no vetor na posição " + i + ":");
+                        vetor[i] = scanner.next();
+                    }
+
+                    //imprime o vetor
+                    System.out.print("O vetor inserido ficou assim: [");
+                    for (int i = 0; i < tamanho; i++) {
+                        System.out.print(vetor[i]);
+                        if (i < tamanho - 1) {
+                            System.out.print(", ");
+                        }
+                    }
+                    System.out.println("]");
+                    break;
+
+                    //opção 2
+                case 2:
+                    if (vetor[0] == null){
+                        System.out.println("Voce não pode acessar essa opção, pois o vetor está vazio");
+                        System.out.println("Volte na opção 1 e preencha o vetor...");
+                    }
+                    else {
+                        for (int i = 0; i < tamanho; i++) {
+                            System.out.println("vetor[" + i + "] = " + vetor[i]);
+                        }
+                    }
+                    break;
+
+                    //opcao 3
+                case 3:
+                    // Operação 3.
+                    if (vetor[0] == null){
+                        System.out.println("O vetor está vazio, volte na opção 1 e preencha o vetor para poder buscar um indice");
+                    }
+                    else {
+                        System.out.println("Insira um índice para encontrar o elemento desejado:");
+                        int i = scanner.nextInt();
+                        if (i >= 0 && i < tamanho) {
+                            String elemento = vetor[i];
+                            System.out.println("O elemento encontrado no índice " + i + " foi o " + elemento);
+                        } else {
+                            System.out.println("O índice inserido está inválido. Insira um índice válido.");
+                            System.out.println("Insira um índice para encontrar o elemento desejado:");
+                            int e = scanner.nextInt();
+                            if (e >= 0 && e < tamanho) {
+                                String elemento = vetor[e];
+                                System.out.println("O elemento encontrado no índice " + e + " foi o " + elemento);
+                            }
+                        }
+                    }
+                    break;
+
+                    //opcao 4
+                case 4:
+                int indiceRemover = scanner.nextInt();
+
+                if (vetor[0] == null){
+                    System.out.println("O vetor está vazio, volte na opção 1 e preencha");
                 }
-            } else if (operacao == 2) {
-                //operacao 2.
-                for (int i = 0; i < tamanho; i++) {
-                    System.out.println("vetor[" + i + "] = " + vetor[i]);
+                else {
+                    if (indiceRemover >= 0 && indiceRemover < tamanho) {
+
+                        for (int i = indiceRemover; i < tamanho - 1; i++) {
+                            vetor[i] = vetor[i + 1];
+                        }
+                        vetor[tamanho - 1] = null;
+
+                        String[] novoVetor = new String[tamanho - 1];
+                        for (int i = 0; i < novoVetor.length; i++) {
+                            novoVetor[i] = vetor[i];
+                        }
+                        vetor = novoVetor;
+
+                        int tamanho2 = tamanho;
+
+                        System.out.print("O vetor final ficou assim: [");
+                        for (int i = 0; i < tamanho2 - 1; i++) {
+                            System.out.print(vetor[i]);
+                            if (i < tamanho2 - 2) {
+                                System.out.print(", ");
+                            }
+                        }
+                        System.out.println("]");
+                    } else {
+                        System.out.println("Índice inválido. Insira um valor válido.");
+                    }
                 }
-            } else if (operacao == 3) {
-                //operacao 3.
-                System.out.println("Insira um índice para encontrar o elemento desejado:");
-                int i = scanner.nextInt();
-                if (i >= 0 && i < tamanho) {
-                    String elemento = vetor[i];
-                    System.out.println("O elemento encontrado no índice " + i + " foi o " + elemento);
-                } else {
-                    System.out.println("O índice inserido está inválido, insira um índice valido:");
-                }
+                break;
+
+                //opção 5
+                case 5:
+                    System.out.println("Encerrando o programa...");
+                    return;
+
+                default:
+                    System.out.println("Operação inválida. Insira um valor válido (1 a 5).");
+                    break;
             }
+
             //operação 5.
         } while (operacao != 5);
         }
