3fe5a7ee092df78c0f82b30db3140d1acc79daf2
p1nheiros <pinheiros.dev@gmail.com>
2023-06-23 22:09:41 -0300

    desafio

---
+++ intellij/ProjetosJava/src/com/company/Desafio.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Desafio {
+
+    public static void mostrarMatriz(String[][] matriz, int linhas, int colunas) {
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void preencherMatriz(String[][] matriz, int linhas, int colunas) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.println("Insira uma informação textual na matriz:");
+                matriz[i][j] = scanner.nextLine();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int qtdLinhas = 5;
+        int qtdColunas = 3;
+        String [][] listaDeCompra = new String[qtdLinhas][qtdColunas];
+        System.out.println("Matriz após a sua criação:");
+        mostrarMatriz(listaDeCompra, qtdLinhas, qtdColunas);
+
+        preencherMatriz(listaDeCompra, qtdLinhas, qtdColunas);
+        System.out.println("Matriz após preenchimento");
+        mostrarMatriz(listaDeCompra, qtdLinhas, qtdColunas);
+
+
+
+    }
+}
